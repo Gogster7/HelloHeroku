2b1edcfd20231439d62aa65addcdf8dfa92a7504
Gogster7 <45371473+Gogster7@users.noreply.github.com>
2020-10-20 19:14:37 -0400

    Add files via upload

--- src/main/java/servlet/AttributeServlet.java
+++ src/main/java/servlet/AttributeServlet.java
@@ -18,9 +18,12 @@ public void doGet (HttpServletRequest request, HttpServletResponse response)
    // Get session object
    HttpSession session = request.getSession();
 
+   String action = request.getParameter("action");
    String name   = request.getParameter("attrib_name");
    String value  = request.getParameter("attrib_value");
    String remove = request.getParameter("attrib_remove");
+   String album  = request.getParameter("attrib_album");
+   String song = request.getParameter("attrib_song");
 
    if (remove != null && remove.equals("on"))
    {
@@ -32,8 +35,40 @@ public void doGet (HttpServletRequest request, HttpServletResponse response)
       {
          session.setAttribute(name, value);
       }
+      if ((album != null && album.length() > 0) && (song != null && song.length() > 0))
+      {
+         session.setAttribute(album, song);
+      }
 
    }
+   
+   if (action != null && action.equals("invalidate"))
+   {  // Called from the invalidate button, kill the session.
+      // Get session object
+      HttpSession session = request.getSession();
+      session.invalidate();
+
+      response.setContentType("text/html");
+      PrintWriter out = response.getWriter();
+
+      out.println("<html>");
+      out.println("<head>");
+      out.println(" <title>Session lifecycle</title>");
+      out.println("</head>");
+      out.println("");
+      out.println("<body>");
+
+      out.println("<p>Your session has been invalidated.</P>");
+
+      // Create a link so the user can create a new session.
+    
+      out.println("<a href=\"" + lifeCycleURL + "?action=newSession\">");
+      out.println("Create new session</A>");
+
+      out.println("</body>");
+      out.println("</html>");
+      out.close();
+   } //end if
 
    response.setContentType("text/html");
    PrintWriter out = response.getWriter();
@@ -60,10 +95,19 @@ public void doGet (HttpServletRequest request, HttpServletResponse response)
    out.println(" Value: ");
    out.println(" <input type=\"text\" size=\"10\" name=\"attrib_value\">");
 
+   out.println(" Album: ");
+   out.println(" <input type=\"text\" size=\"10\" name=\"attrib_album\">");
+
+   out.println(" Song: ");
+   out.println(" <input type=\"text\" size=\"10\" name=\"attrib_song\">");
+
    out.println(" <br><input type=\"checkbox\" name=\"attrib_remove\">Remove");
    out.println(" <input type=\"submit\" name=\"update\" value=\"Update\">");
    out.println("</form>");
    out.println("<hr>");
+   
+   out.print  ("<br><br><a href=\"" + lifeCycleURL + "?action=invalidate\">");
+   out.println("Invalidate the session</a>");
 
    out.println("Attributes in this session:");
    Enumeration e = session.getAttributeNames();
@@ -71,11 +115,17 @@ public void doGet (HttpServletRequest request, HttpServletResponse response)
    {
       String att_name  = (String) e.nextElement();
       String att_value = (String) session.getAttribute(att_name);
+      String att_album  = (String) e.nextElement();
+      String att_song = (String) session.getAttribute(att_album)
 
       out.print  ("<br><b>Name:</b> ");
       out.println(att_name);
       out.print  ("<br><b>Value:</b> ");
       out.println(att_value);
+      out.print  ("<br><b>Album:</b> ");
+      out.println(att_album);
+      out.print  ("<br><b>Song:</b> ");
+      out.println(att_song);
    } //end while
 
    out.println("</body>");
